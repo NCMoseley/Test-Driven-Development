5a98a752db590dee0528487847e5e1f65ba349ba
NCMoseley <ncmoseley@gmail.com>
2018-02-22 01:39:11 -0800

    Editsaway almost working, tests added

--- __tests__/editsAway.spec.js
+++ __tests__/editsAway.spec.js
@@ -3,7 +3,7 @@
 const editsAway = require("../lib/editsAway");
 
 myStr1 = "pale";
-myStr2 = "plex";
+myStr2 = "patl";
 
 describe("Is input an object?", () => {
 	test("Should stop the function if input is not a string", () => {
@@ -13,9 +13,10 @@ describe("Is input an object?", () => {
 	});
 });
 
-// describe("Is input an empty array?", () => {
-// 	test("Should return warning", () => {
-// 		const result = editsAway([]);
-// 		expect(result).toBe("Not an object, or empty array");
-// 	});
-// });
+describe("Is input an empty array?", () => {
+	test("Should return warning", () => {
+		// const result = editsAway([]);
+		const result = editsAway(myStr1, myStr2);
+		expect(result).not.toBe("Error, you must enter a string");
+	});
+});
